3abde278325980d0e8c9f7b00ec1cf616018089e
mrinalsaurabh <msaurabh92@gmail.com>
2017-11-17 08:43:49 +0530

    [Mrinal] | Some progress on the save query

--- src/clj/newsback_api/data/news.clj
+++ src/clj/newsback_api/data/news.clj
@@ -25,18 +25,6 @@
           desc=There was no news corresponding to the month requested.")
         )))))
 
-; (defn- upsert-to-news-table-query [news-id news]
-;   (format "INSERT INTO newsdetails (news)
-;             VALUES ('%s')
-;             ON CONFLICT (id)
-;             DO UPDATE SET news = '%s'"
-;           country-id, plan-month, plan-year, plan-data, plan-data))
-
-; (defn- insert-into-changelog-table-query [country-id plan-month plan-year plan-data user]
-;   (format "INSERT INTO changelog (country_id, plan_month, plan_year, change, changed_by, created_at)
-;             VALUES ('%s', %s, '%s', '%s', '%s', current_timestamp)"
-;           country-id, plan-month, plan-year, plan-data (:user-id user)))
-
 ; (defn- to-db [plan]
 ;   (walk/postwalk-replace {:new-hires :new_hires :global-movements :global_movements} plan))
 
@@ -47,3 +35,12 @@
 ;     (q/execute (upsert-to-supply-plan-table-query country-id plan-month plan-year plan-json)
 ;                (insert-into-changelog-table-query country-id plan-month plan-year plan-json user))
 ;      (log/debug (format "op=save-plan, status=OK, desc=Inserted/Updated supply plan with country-id '%s' for month '%s' and year '%s' ." country-id plan-month plan-year))))
+
+(defn save-news [id news-html user-id]
+  (let [news-id (if id id "default")
+        query (if-not id "INSERT INTO newsdetails VALUES (default, '%s');
+                          INSERT INTO changelog SELECT nextval('changelog_id_seq'), '%s'
+                          currval('newsdetails_id_seq'),true;
+                          INSERT INTO changeevents SELECT nextval('changeevents_id_seq'),
+                          currval('changelog_id_seq')")]
+    ))
